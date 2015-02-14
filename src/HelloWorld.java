86d82860c5dc8a0c6c8699113d6c5cd88c457049
John Kevin C. Zurita <bebin.singkit@gmail.com>
2015-02-14 14:58:41 +0800

    Deleted 3 lines of code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
 	void helloWorld() {
 		System.out.println("Hello World");
-		System.out.println("Ryan Test");
-		System.out.println("Kevin Test");
-		System.out.println("Test 6");
 	}
 }
