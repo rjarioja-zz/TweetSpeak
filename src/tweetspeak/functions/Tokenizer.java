cd26ad4cccc33bba110510b5757cbf7faa26dc5f
Ryan A <rjarioja@gmail.com>
2015-02-19 00:14:14 +0800

    Created enums

--- src/tweetspeak/functions/Tokenizer.java
+++ src/tweetspeak/functions/Tokenizer.java
@@ -1,5 +1,15 @@
 package tweetspeak.functions;
 
-public class Tokenizer {
+import java.util.LinkedList;
+import tweetspeak.objects.Token;
 
+public class Tokenizer {
+	private String sourceCode;
+	private LinkedList<Token> tokens = new LinkedList<Token>();
+	
+	//getters
+	public String getSourceCode() { return sourceCode; }
+	
+	//setters
+	public void setSourceCode(String token) { this.sourceCode = token; }
 }
