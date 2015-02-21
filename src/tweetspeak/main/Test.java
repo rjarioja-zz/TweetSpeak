55ef4fd849695e130e480d97ec36fba786e463a9
Ryan A <rjarioja@gmail.com>
2015-02-22 05:39:43 +0800

    Modified UI

--- src/tweetspeak/main/Test.java
+++ src/tweetspeak/main/Test.java
@@ -19,36 +19,44 @@ public class Test implements ActionListener {
 	private JScrollPane scrollPane;
 	private JPanel panel1, panel2;
 	private JTextArea textArea;
-	private JButton buttonLoad, buttonSave, buttonRun, buttonClose;
-	private JFileChooser browser = new JFileChooser();
+	private JButton buttonLoad, buttonSave, buttonExit;
+	private JButton buttonTokenizer, buttonParser, buttonInterpreter;
+	private JFileChooser load = new JFileChooser();
+	private JFileChooser save = new JFileChooser();
 	
 	public Test() {
 		frame = new JFrame(title);
 		panel1 = new JPanel();
 		panel2 = new JPanel();
 		textArea = new JTextArea("");
-		buttonLoad = new JButton("Load");
-		buttonSave = new JButton("Save");
-		buttonRun = new JButton("Run");
-		buttonClose = new JButton("Close");
+		buttonLoad = new JButton("Load Source Code");
+		buttonSave = new JButton("Save Source Code");
+		buttonExit = new JButton("Exit Program");
+		buttonTokenizer = new JButton("Tokenize");
+		buttonParser = new JButton("Parse");
+		buttonInterpreter = new JButton("Interprete");
+		
 	}
 	
 	public void launchApp() {
-		textArea.setPreferredSize(new Dimension(700,500));
+		
 		textArea.setFont(new java.awt.Font("Consolas", 0, 14));
 		textArea.setTabSize(2);
 		
 		scrollPane = new JScrollPane(textArea);
+		scrollPane.setPreferredSize(new Dimension(800,600));
 		scrollPane.setWheelScrollingEnabled(true);
 		
 		panel1.setLayout(new BorderLayout());
 		panel1.add(scrollPane, BorderLayout.CENTER);
 		
-		panel2.setLayout(new GridLayout(1,4));
+		panel2.setLayout(new GridLayout(2,3));
 		panel2.add(buttonLoad);
 		panel2.add(buttonSave);
-		panel2.add(buttonRun);
-		panel2.add(buttonClose);
+		panel2.add(buttonExit);
+		panel2.add(buttonTokenizer);
+		panel2.add(buttonParser);
+		panel2.add(buttonInterpreter);
 		
 		frame.setLayout(new BorderLayout());
 		frame.add(panel1, BorderLayout.CENTER);
@@ -58,8 +66,15 @@ public class Test implements ActionListener {
 		
 		buttonLoad.addActionListener(this);
 		buttonSave.addActionListener(this);
-		buttonRun.addActionListener(this);
-		buttonClose.addActionListener(this);
+		buttonExit.addActionListener(this);
+		buttonTokenizer.addActionListener(this);
+		/*buttonRun.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		SeeOutputActionPerformed(arg0);
+        	}
+        });*/
+		buttonParser.setEnabled(false);
+		buttonInterpreter.setEnabled(false);
 		
 		frame.addWindowListener(new CloseHandler());
 	}
@@ -71,9 +86,9 @@ public class Test implements ActionListener {
 	
 		if (textArea.getText() != null) {
 			if (source == buttonLoad) {
-				browser.showOpenDialog(frame);
-                sourceFile = browser.getSelectedFile();
-                filename = browser.getSelectedFile().toString();
+				load.showOpenDialog(frame);
+                sourceFile = load.getSelectedFile();
+                filename = load.getSelectedFile().getName();
                 frame.setTitle(title + " - " + filename);
                 try {
 					BufferedReader read = new BufferedReader(new FileReader(sourceFile));
@@ -94,6 +109,9 @@ public class Test implements ActionListener {
 			}
 			
 			else if (source == buttonSave) {
+				save.showSaveDialog(frame);
+				sourceFile = save.getSelectedFile();
+				filename = save.getSelectedFile().getName();
 				try {
 					PrintWriter write = new PrintWriter(new FileWriter(sourceFile, false));
 					write.print(textArea.getText());
@@ -102,12 +120,15 @@ public class Test implements ActionListener {
 				catch (IOException ie) {}
 			}
 			
-			else if (source == buttonRun) {
-				System.out.print(Code.toLines());
-				Tokenizer.tokenize(Code.getLine(0));
+			else if (source == buttonTokenizer) {
+//				System.out.print(Code.toLines());
+//				Tokenizer.tokenize(Code.getLine(0));
+				Code.setCode(textArea.getText());
+				TokenOutput tokenOutput = new TokenOutput();
+				tokenOutput.launchApp();
 			}
 			
-			else if (source == buttonClose) System.exit(0);
+			else if (source == buttonExit) System.exit(0);
 		}
 	}
 	
