3ecbba893af8855795318bc03f69c990edf16a84
Fhrey <Zeke@Fhrey>
2015-02-22 03:23:35 +0800

    merge

--- src/tweetspeak/functions/Tokenizer.java
+++ src/tweetspeak/functions/Tokenizer.java
@@ -38,10 +38,9 @@ public class Tokenizer {
 					 * OPERATORS - ARITHMETIC
 					 */
 					case '+':
-						
-						if (index + 1 < sourceCode.length() && sourceCode.charAt(index + 1) == '+') {
-							token += sourceCode.charAt(index++); count++;
-							System.out.println(index + " " + count);
+						token += sourceCode.charAt(index++); count++;
+						if (index < sourceCode.length() && sourceCode.charAt(index) == '+') {
+							token += sourceCode.charAt(index); count++;
 							System.out.print(token + ".INC_OP");
 							//create token
 							t = new Token(
@@ -111,6 +110,7 @@ public class Tokenizer {
 							        if (sourceCode.charAt(index) == 'o' && sourceCode.charAt(index + 1) == 'g') {
 							        	token += "og"; count += 2; index += 2;
 							            switch (sourceCode.charAt(index)) {
+							            	//login
 							                case 'i': 
 							                    token += sourceCode.charAt(index++); count++;
 							                    if (sourceCode.charAt(index) == 'n') {  
@@ -172,8 +172,187 @@ public class Tokenizer {
 							        }
 							    }
 							    
+							//#comment start ---ZEKE
+							case 'c': 
+								
+									token += sourceCode.charAt(index++); count++;
+									//if-level-1
+									if(sourceCode.charAt(index) == 'o' && sourceCode.charAt(index + 1) == 'm' && sourceCode.charAt(index+2) == 'm' 
+											&& sourceCode.charAt(index+3) == 'e' && sourceCode.charAt(index+4) == 'n' && sourceCode.charAt(index+5) == 't') {
+									   token += "omment"; count += 6; index += 6;
+									   System.out.print(token + ".COMMENT");
+									   continue;
+									}
+									else {
+								       index --; 
+								       System.out.print(token + ".ERROR");
+								       continue;
+								    }
+									
+
+								
+							//#comment end; ---END
+							//#newsfeed start 
+							case 'n':
+								
+									token += sourceCode.charAt(index++); count++;
+									if(sourceCode.charAt(index) == 'e' && sourceCode.charAt(index + 1) == 'w' && sourceCode.charAt(index + 2) == 's'
+											&& sourceCode.charAt(index + 3) == 'f' && sourceCode.charAt(index + 4) == 'e' && sourceCode.charAt(index + 5) == 'e' 
+											&& sourceCode.charAt(index + 6) == 'd') {
+										token += "ewsfeed"; count += 7; index += 7;
+										
+										if (index == sourceCode.length()) {
+											System.out.print(token + ".MAIN");
+											continue;
+										}
+										else {
+											 index --;
+											 System.out.print(token + ".ERROR");
+											 continue;
+										}
+									}
+									else {
+										 index --;
+										 System.out.print(token + ".ERROR");
+										 continue;
+									}
+						
+							//#newsfeed END
+							case 's':
+									
+									token += sourceCode.charAt(index++); count++;
+									switch(sourceCode.charAt(index)) {
+									//#SHARE
+										case 'h':
+											token += sourceCode.charAt(index++); count++;
+											if(sourceCode.charAt(index) == 'a' && sourceCode.charAt(index + 1) == 'r' && sourceCode.charAt(index + 2) == 'e'){
+												token += "are"; index += 3; count += 3;
+	
+													System.out.print(token + ".ASSIGN");
+													continue;
+												}	
+											else {
+												index --;
+												System.out.print(token + ".ERROR");
+												continue;
+											}
+										
+										//#STATUS
+										case 't':
+											token += sourceCode.charAt(index++); count++;
+											if(sourceCode.charAt(index) == 'a' && sourceCode.charAt(index + 1) == 't' && sourceCode.charAt(index + 2) == 'u' &&
+													sourceCode.charAt(index + 3) == 's'){
+												token += "atus"; index += 4; count += 4;
+													System.out.print(token + ".WHILE");
+													continue;
+											}
+											else {
+												index --;
+												System.out.print(token + ".ERROR");
+												continue;
+											}
+									}
+							
+							case 'o': 
+									token += sourceCode.charAt(index++); count++;
+									if(sourceCode.charAt(index) == 'o' && sourceCode.charAt(index + 1) == 't') {
+										token += "ot"; index +=2; count +=2;
+										/* DATA 
+										 * TYPES
+										 */
+										switch(sourceCode.charAt(index)) {
+											case 'i': 
+												token += sourceCode.charAt(index++); count++;
+												if (index < sourceCode.length() && sourceCode.charAt(index) == ' ') {
+													token += sourceCode.charAt(index++);
+													System.out.print(token + ".DATATYPE_INT");
+													continue;
+												}
+												else {
+													index --;
+													System.out.print(token + ".ERROR");
+													continue;
+												}
+											case 'f':
+												token += sourceCode.charAt(index++); count++;
+												if (index < sourceCode.length() && sourceCode.charAt(index) == ' ') {
+													token += sourceCode.charAt(index++);
+													System.out.print(token + ".DATATYPE_FLOAT");
+													continue;
+												}
+												else {
+													index --;
+													System.out.print(token + ".ERROR");
+													continue;
+												}
+											case 'c':
+												token += sourceCode.charAt(index++); count++;
+												if (index < sourceCode.length() && sourceCode.charAt(index) == ' ') {
+													token += sourceCode.charAt(index++);
+													System.out.print(token + ".DATATYPE_CHAR");
+													continue;
+												}
+												else {
+													index --;
+													System.out.print(token + ".ERROR");
+													continue;
+												}
+											case 's':
+												token += sourceCode.charAt(index++); count++;
+												if (index < sourceCode.length() && sourceCode.charAt(index) == ' ') {
+													token += sourceCode.charAt(index++);
+													System.out.print(token + ".DATATYPE_STRING");
+													continue;
+												}
+												else {
+													index --;
+													System.out.print(token + ".ERROR");
+													continue;
+												}
+											case 'b':
+												token += sourceCode.charAt(index++); count++;
+												if (index < sourceCode.length() && sourceCode.charAt(index) == ' ') {
+													token += sourceCode.charAt(index++);
+													System.out.print(token + ".DATATYPE_BOOL");
+													continue;
+												}
+												else {
+													index --;
+													System.out.print(token + ".ERROR");
+													continue;
+												}
+											case 'v':
+												token += sourceCode.charAt(index++); count++;
+												if (index < sourceCode.length() && sourceCode.charAt(index) == ' ') {
+													token += sourceCode.charAt(index++);
+													System.out.print(token + ".DATATYPE_VOID");
+													continue;
+												}
+												else {
+													index --;
+													System.out.print(token + ".ERROR");
+													continue;
+												}
+										}
+									}
+									
+									//#OUTBOX
+									else if(sourceCode.charAt(index) == 'u' && sourceCode.charAt(index + 1) == 't' && sourceCode.charAt(index + 2) == 'b'
+											&& sourceCode.charAt(index + 3) == 'o' && sourceCode.charAt(index + 4) == 'x') {
+										
+										token += "utbox"; index += 5; count += 5;
+										System.out.print(token + ".OUTPUT");
+									}
+									else {
+										index --;
+										System.out.print(token + ".ERROR");
+										continue;
+									}
+									break;
+							
 							default:
-							    System.out.print(" level 2 exit");
+							    //reserved words
+								System.out.print(" level 2 exit");
 							    break;
 						}
 						
@@ -196,7 +375,7 @@ public class Tokenizer {
 	
 	// TESTING
 	public static void main(String args[]) {
-		Tokenizer.tokenize(new CodeLine("#login +++", 1));
+		Tokenizer.tokenize(new CodeLine("#ootv #oots #ootc #ootb #comment #share #status #ooti #ootf #outbox ", 1));
 	}
 		
 }	
