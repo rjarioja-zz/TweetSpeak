648e953e6aaa44748e320aa834c0f65427c42e67
yankee <yankee@192.168.1.12>
2015-03-27 17:48:06 +0800

    Yankee

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		System.out.println("Git Edit on MacLab no. 17");
 		System.out.println("Hi Janice! :P");
 		System.out.println("Hi Kim!");
+		System.out.println("Lol");
 	}
 }
