64ffa562a5046cf3ec08fe4217553c0ca20a6e66
rjarioja <rjarioja@gmail.com>
2015-04-04 16:53:11 +0800

    Updated Parser.java, Node.java. Studied Parser (if that even qualifies as a commit message)

--- src/tweetspeak/objects/Node.java
+++ src/tweetspeak/objects/Node.java
@@ -1,79 +1,49 @@
 package tweetspeak.objects;
 
 import java.util.ArrayList;
-
-import tweetspeak.collections.TokenType;;
+import tweetspeak.collections.TokenType;
+import tweetspeak.collections.TokenName;
 
 public class Node {
 
 	private Node parentNode;
 	private ArrayList<Node> child = new ArrayList<Node>();
-	private TokenType tokenType;
-	private Object value;
-	private String tokenData;
 	private Token token = null;
-
-	public Token getToken() {
-		return token;
-	}
-	public void setToken(Token token) {
-		this.token = token;
-	}
-
-	public Node getParentNode() {
-		return parentNode;
-	}
-
-	public void setParentNode(Node parentNode) {
-		this.parentNode = parentNode;
-	}
-
-	public String getTokenData() {
+	private TokenType tokenType;
+	private TokenName tokenName;
+	private String tokenData = "";
+	private Object value;
+	
+	//getters
+	public Object getValue() { return value; }
+	
+	public Node getParentNode() { return parentNode; }
+	public ArrayList<Node> getchild() { return child; }
+	public Node getChild(int index) { return child.get(index); }
+	
+	public Token getToken() { return token; }
+	public TokenType getTokenType() { return tokenType; }
+	public TokenName getTokenName() { return tokenName; }
+	public String getTokenData() { 
+		if (token != null) return token.getName();
 		return tokenData;
 	}
-
-	public void setTokenData(String tokenData) {
-		this.tokenData = tokenData;
-	}
-	public ArrayList<Node> getchild() {
-		return child;
-	}
-
-	public void addChild(Node childNode) {
-		child.add(childNode);
-	}
-
-	public void addChild(int index, Node childNode) {
-		child.add(index, childNode);
-	}
-
-	public Node getChild(int index) {
-		return child.get(index);
-	}
-
-	public void setChild(Node previousChild, Node newChild) {
-		child.set(child.indexOf(previousChild), newChild);
-	}
-
-	public void setchild(ArrayList<Node> child) {
-		this.child = child;
-	}
-
-	public TokenType TokenType() {
-		return tokenType;
-	}
-
-	public void setTokenType(TokenType tokenType) {
-		this.tokenType = tokenType;
-	}
-
-	public Object getValue() {
-		return value;
-	}
-
-	public void setValue(Object value) {
-		this.value = value;
-	}
+	
+	//setters
+	public void setValue(Object value) { this.value = value; }
+	
+	public void setParentNode(Node parentNode) { this.parentNode = parentNode; }
+	public void setchild(ArrayList<Node> child) { this.child = child; }
+	public void setChild(Node previousChild, Node newChild) { child.set(child.indexOf(previousChild), newChild); }
+	
+	public void setToken(Token token) { this.token = token; }
+	public void setTokenType(TokenType tokenType) { this.tokenType = tokenType; }
+	public void setTokenName(TokenName tokenName) { this.tokenName = tokenName; }
+	public void setTokenData(String tokenData) { this.tokenData = tokenData; }
+	
+	//methods
+	public void addChild(Node childNode) { child.add(childNode); }
+	public void addChild(int index, Node childNode) { child.add(index, childNode); }
 
 	@Override
 	public String toString() {
