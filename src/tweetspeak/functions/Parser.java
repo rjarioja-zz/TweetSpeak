c01eed784b3d4a507b1ff0414762b793609ec072
Andie M. Lara <lara.andreaaa@yahoo.com>
2015-04-19 05:19:06 +0800

    new list nostmtsep

--- src/tweetspeak/functions/Parser.java
+++ src/tweetspeak/functions/Parser.java
@@ -125,7 +125,7 @@ public class Parser {
 	    		TokenName.DIV_OP.toString(),			TokenName.MOD_OP.toString(),
 	    		TokenName.EXP_OP.toString());
 	    
-	    //43 - 73 pero di lahat -- same as checkReduce but WITHOUT else_if & else    
+	    //43 - 73 pero di lahat -- same as checkReduce but WITHOUT else_if & else 
 	    List<String> checkReduce5 = Arrays.asList(	
 	    		TokenName.VAR.toString(), 				TokenName.DEDENT.toString(),
 	    		TokenName.STMT_SEP.toString(), 			TokenName.ASSIGN.toString(),
@@ -181,6 +181,18 @@ public class Parser {
 				TokenName.EQUAL_OP.toString(),			TokenName.NOT_EQUAL_OP.toString(),
 				TokenName.GREAT_OP.toString(),			TokenName.LESS_OP.toString(),
 				TokenName.GREAT_EQ_OP.toString());
+		
+		List<String> nostmtsep = Arrays.asList(	//similar to checkReduce5 but WITHOUT stme_sep for 200th+ states
+	    		TokenName.VAR.toString(), 				TokenName.DEDENT.toString(),
+	    		TokenName.ASSIGN.toString(),			TokenName.PROC_CALL.toString(), 
+	    		TokenName.CONTINUE.toString(),	 		TokenName.BREAK.toString(),
+	    		TokenName.OUTPUT.toString(),			TokenName.DATATYPE_BOOL.toString(),
+	    		TokenName.DATATYPE_CHAR.toString(),		TokenName.DATATYPE_FLOAT.toString(),
+	    		TokenName.DATATYPE_INT.toString(), 		TokenName.DATATYPE_STRING.toString(),
+	    		TokenName.DATATYPE_VOID.toString(), 	TokenName.IF.toString(),
+	    		TokenName.DO.toString(),				TokenName.WHILE.toString(),
+	    		TokenName.INC_OP.toString(), 			TokenName.DEC_OP.toString(),
+	    		TokenName.PROC_RET.toString(),			TokenName.INPUT.toString());
 
 	    getToken();
 	    System.out.println("START PARSE WITH: " + currentToken.toString());
